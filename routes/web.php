f101ec3750a8c54b66c079b9f030ff3b5e630da8
Josias Bueno <messias_jpb@hotmail.com>
2026-05-27 12:21:55 -0300

    Criados links na view welcome

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/basic', 'basic');
+Route::view('/3-basico', '3-basic');
 Route::view('/3-livre', '3-free');
 Route::view('/3-rotacao', '3-rotation');
-Route::view('/4-padrao', '4x4-default');
 Route::view('/4-4-3', '4x4-default');
-Route::view('/5-cheia', '5-full');
 Route::view('/4-3-normal', '4x3-normal');
 Route::view('/4-3-rotacao', '4x3-rotation');
 Route::view('/4-3-bloqueio', '4x4-block');
+Route::view('/4-padrao', '4x4-default');
+Route::view('/5-cheia', '5-full');
